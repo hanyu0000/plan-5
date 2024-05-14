9f52204604c2787e19997a06d83d613879aa6fe4
hanyu0000 <3634802280@qq.com>
2024-05-14 21:51:25 +0800

    first

---
+++ s.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_THREADS 64
+#define MAX_PATH_LEN 257
+#define MAX_SKIP_PATHS 10
+
+pthread_mutex_t mutex;
+
+typedef struct SearchConfig
+{
+    char root_path[MAX_PATH_LEN];                  // 要搜索的根目录
+    char file_type[MAX_PATH_LEN];                  // 要搜索的文件类型，如 ".txt"、".cpp" 等
+    int max_concurrency;                           // 最大并发数
+    int max_depth;                                 // 最大搜索深度
+    bool skip_hidden;                              // 是否跳过隐藏文件或目录
+    char skip_paths[MAX_SKIP_PATHS][MAX_PATH_LEN]; // 要跳过的目录或文件的路径
+} Task;
+
+void count(const char *path, const Task *config, int depth);
+void *thread(void *arg);
